3bb450d20d681215dc3e9e85c8a89c5aa33e9cb0
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-02 23:21:39 +0200

    added sql exercises

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,1 @@
+INSERT INTO hbtn_0c_0.first_table (`id`, `name`) VALUES ('89', 'BEST SCHOOL');
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (id int , name VARCHAR(256), score int);
+
+INSERT INTO hbtn_0c_0.first_table (`id`, `name`, `score`) VALUES
+  (1, 'John', 10),
+  (2, 'Alex', 3),
+  (3, 'Bob', 14),
+  (4, 'George', 8);
