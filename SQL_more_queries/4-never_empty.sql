3b0b5afbf0efe2d83c29ba8a450f7ea84b0f4b5f
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-03 18:07:53 +0200

    CREATING USERS AND GRANTING PRIVILEGES

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- write a script that creates a table called id_not_null in the current database hbtn_0d_2
-CREATE TABLE IF NOT EXISTS id_not_null (id int int DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
