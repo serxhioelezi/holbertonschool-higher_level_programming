85b1df0b1e9d19fe1776a65882f0a1cffca25288
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-03 14:17:40 +0200

    changed readme

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- script to create a table first_table
-INSERT INTO first_table (id, name) VALUES (89, 'BEST SCHOOL');
+-- script to create a row first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
