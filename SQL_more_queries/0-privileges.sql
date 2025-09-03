8eb246102f772e7fe085f71413f13ac5c7fc7377
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-03 17:37:55 +0200

    CREATING USERS AND GRANTING PRIVILEGES

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Show the privileges of the users created in the previous exercise
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
