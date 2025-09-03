188c097de5a5fa7c3277694d189c1c3bed988123
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-03 18:16:29 +0200

    CREATING USERS AND GRANTING PRIVILEGES

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- write a script that creates a database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 -- write a script that creates a table called states in the current database hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL, NAME VARCHAR(256) NOT NULL);
