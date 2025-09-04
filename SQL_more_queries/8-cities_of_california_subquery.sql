02f088affd2bbb50cae2839f40c4635bf058bab7
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-04 18:49:39 +0200

    CREATING USERS AND GRANTING PRIVILEGES

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,3 @@
 -- whithout using joins
-SELECT * FROM cities WHERE state_id=(
-    SELECT id FROM states WHERE name='California'
-)
+SELECT * FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California')
 ORDER BY id ASCHENDING;
