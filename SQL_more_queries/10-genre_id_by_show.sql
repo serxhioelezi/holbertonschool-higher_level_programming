4c3209ba2fa8c0ed794f4988e554b4418bbdbfdb
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-04 19:02:33 +0200

    CREATING USERS AND GRANTING PRIVILEGES

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
--- Query to list all TV show titles with their corresponding genre IDs
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- Query to list all TV shows with their genre IDs
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
